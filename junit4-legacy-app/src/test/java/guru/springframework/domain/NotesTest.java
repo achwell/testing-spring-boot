fdf2fc8f01131739c64f4fa17e5599016fc5f675
Axel Wulff Sæther <axelwulff@mac.com>
2021-08-12 14:14:25 +0200

    Upgraded junit4 -> junit5

---
+++ junit4-legacy-app/src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void getRecipe() {
+    }
+
+    @Test
+    void getRecipeNotes() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setRecipe() {
+    }
+
+    @Test
+    void setRecipeNotes() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
