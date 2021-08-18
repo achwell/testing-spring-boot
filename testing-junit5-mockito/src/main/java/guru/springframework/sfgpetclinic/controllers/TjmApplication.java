908a8d40b996e487546bfb6b363c09e28ec0314a
Axel Wulff Sæther <axelwulff@mac.com>
2021-08-18 15:13:09 +0200

    modified:   .gitignore 	modified:   pom.xml 	new file:   sfg-restdocs-example 	modified:   testing-junit5-mockito/pom.xml 	new file:   testing-junit5-mockito/src/main/java/guru/springframework/sfgpetclinic/controllers/TjmApplication.java 	deleted:    tsbb-sfg-brewery/.gitignore

---
+++ testing-junit5-mockito/src/main/java/guru/springframework/sfgpetclinic/controllers/TjmApplication.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TjmApplication.class, args);
+    }
+}
